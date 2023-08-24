9f8dd7a688176bbfcedd958305ca5f5b87a1cd03
burak <burakgokkce@gmail.com>
2023-08-24 23:05:00 +0300

    say hello test

--- ExApp/ViewController.swift
+++ ExApp/ViewController.swift
@@ -11,7 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let sayhello  = "hello world"
+        print(sayhello)
+
     }
 
 
